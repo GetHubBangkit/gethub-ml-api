71599d8802dc26a1a8d5f2c4e96637cee691d5b3
Arman Juliansyah <arman.liansyah07@gmail.com>
2024-06-04 01:31:52 +0700

    change config db, tune predict fraud job

--- configs/config.py
+++ configs/config.py
@@ -1,27 +1,11 @@
 from sqlalchemy import create_engine
 
-# DB_USERNAME = "root"
-# DB_PASSWORD = ""
-# DB_DATABASE = "db_gethub"
-# DB_HOST = "localhost"
-
-# DB_USERNAME = "root"
-# DB_PASSWORD = "MGEIQHGNLQeLRfw"
-# DB_DATABASE = "db_gethub"
-# DB_HOST = "34.101.220.106"
-
-DB_USERNAME = "avnadmin"
-DB_PASSWORD = "AVNS_cI7SxM7BMBa3H8flayO"
+DB_USERNAME = "root"
+DB_PASSWORD = ""
 DB_DATABASE = "db_gethub"
-DB_HOST = "gethub-mysql-gethub.g.aivencloud.com"
+DB_HOST = "localhost"
 DB_PORT = 12469
-
-SECRET_KEY = "UZJ!y^t*=g@Bj&b?Y5tkF%v!!P8R@Wm$E"
-
-# def get_database_engine():
-#     SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}"
-#     engine = create_engine(SQLALCHEMY_DATABASE_URL)
-#     return engine
+SECRET_KEY = ""
 
 def get_database_engine():
     SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
